fa8c5b886bf73ecc0d1d3728ea9e8d5439c59ca5
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-08-13 10:36:04 +0000

    [finish]ヘッダーテキスト記述まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
