de61caad1bdcedca9255ae3b8860a6ac5e9c6257
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-08-28 10:01:18 +0000

    search in progress

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,19 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+
+  def self.search(search,word)
+    if search == "forward_match"
+      @book = Book.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @book = Book.where("#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("name LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :books, only: [:show, :index, :edit, :create, :destroy, :update]
+
+  get 'search' => 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
