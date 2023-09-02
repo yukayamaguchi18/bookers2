9b9a85694923564b35c74020208a037cb5c64310
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-09-02 06:17:54 +0000

    応用課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
+    book_comment.book_id = @book.id
     unless book_comment.comment.blank?
       book_comment.save
     else
-      @book = Book.find(params[:book_id])
       redirect_back fallback_location: root_path, notice: 'Comment cannot be blank.'
     end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     book_comment.delete
   end
